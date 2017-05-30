69d1d7c9107ef7dd732ad464af39726b29185a74
vahit <vahit.serif119@gmail.com>
2017-05-31 00:35:05 +0300

    made some updates

---
+++ src/Cable/Component/Factory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Cable\Container;
+
+/**
+ * Class Factory
+ * @package Cable\Container
+ */
+class Factory
+{
+    /**
+     * create a new container instance
+     *
+     * @param ProviderRepository|null $providerRepository
+     * @return Container
+     */
+    public static function create(ProviderRepository $providerRepository = null)
+    {
+        return new Container(
+            new BoundManager(),
+            new MethodManager(),
+            $providerRepository
+        );
+    }
+}
